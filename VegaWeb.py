bea5a2b01e4786c559595352c179aa565d862d42
Icarus <icarus@novus.io>
2017-10-18 14:38:55 -0400

    Updated to newer method (sqla)

--- VegaWeb.py
+++ VegaWeb.py
@@ -7,12 +7,12 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///VegaWeb.db'
 db = SQLAlchemy(app)
 
+db.create_all()
 
 @app.route('/')
 def index():
     output = render_template('index.html')
     return output
-
 
 app.run(debug=True,host='0.0.0.0',port=1900,threaded=True)
 
--- models.py
+++ models.py
@@ -1,12 +1,10 @@
-from VegaWeb import db_session,Base
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String
+from VegaWeb import db
 
-class User(Base):
+class User(db.model):
     __tablename__ = 'user'
     user_id = Column(Integer, primary_key=True)
     email = Column(String(80))
-
-
 
     def __repr__(self):
         """docstring for __repr__"""
