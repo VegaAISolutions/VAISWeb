cd2cfbb2c4b855234ead7792f778179dc0e38790
Icarus <icarus@novus.io>
2017-10-18 14:20:29 -0400

    Updated to newer method (python3)

--- VegaWeb.py
+++ VegaWeb.py
@@ -1,8 +1,12 @@
 from flask import Flask
+from flask import render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-from flask import render_template
-# from mandrill import drill
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///VegaWeb.db'
+db = SQLAlchemy(app)
+
 
 @app.route('/')
 def index():
