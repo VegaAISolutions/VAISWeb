c218c1aee69378d8e40a320986f267e90595fa1c
Icarus <icarus@novus.io>
2017-10-19 13:23:49 -0400

    Blog feed now pulls from medium

--- VegaWeb.py
+++ VegaWeb.py
@@ -1,9 +1,34 @@
 from flask import Flask
 from flask import render_template
 from flask_sqlalchemy import SQLAlchemy
+import feedparser
 # from models import User //to be used later
+from html.parser import HTMLParser
+
+
+class MLStripper(HTMLParser):
+    def __init__(self):
+        self.reset()
+        self.strict = False
+        self.convert_charrefs= True
+        self.fed = []
+
+    def handle_data(self, d):
+        self.fed.append(d)
+
+    def get_data(self):
+
+        return ''.join(self.fed)
+
+
+def strip_tags(html):
+    s = MLStripper()
+    s.feed(html)
+    return s.get_data()
 
 app = Flask(__name__)
+
+app.jinja_env.filters['strip_tags'] = strip_tags
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///VegaWeb.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = 0
@@ -12,9 +37,12 @@ db = SQLAlchemy(app)
 db.create_all()
 
 
+d = feedparser.parse('https://medium.com/feed/@VegaAISolutions')
+
+
 @app.route('/')
 def index():
-    output = render_template('index/index.html')
+    output = render_template('index/index.html', rss=d, strip_tags=strip_tags)
     return output
 
 app.run(debug=True,host='0.0.0.0',port=1900,threaded=True)
