7010967f9f2f57a84d80afc0dfb33fb7568628f4
Icarus <icarus@novus.io>
2017-10-19 11:32:29 -0400

    Visual changes

--- static/assets/js/app.js
+++ static/assets/js/app.js
@@ -85,11 +85,11 @@ particlesJS('banar',
       "detect_on": "canvas",
       "events": {
         "onhover": {
-          "enable": true,
+          "enable": false,
           "mode": "repulse"
         },
         "onclick": {
-          "enable": true,
+          "enable": false,
           "mode": "push"
         },
         "resize": true
