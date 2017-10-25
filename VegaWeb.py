1a76c6f84247ca5b4fdb0ff3515cf5fad8553bc9
Icarus <icarus@novus.io>
2017-10-25 12:47:29 -0500

    added crowdfund deposit page

--- VegaWeb.py
+++ VegaWeb.py
@@ -57,4 +57,9 @@ def whitepaper():
     return output
 
 
+@app.route('/deposit')
+def deposit():
+    output = render_template('crowdfund/deposit.html')
+    return output
+
 app.run(debug=True,host='0.0.0.0',port=1900,threaded=True)
