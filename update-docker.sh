8bb8b3cb25e5bf0044ebb3e74f0a11263563be2c
slapglif <kurosama112@gmail.com>
2017-10-26 13:05:45 -0500

    Update update-docker.sh

--- update-docker.sh
+++ update-docker.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 sudo -s;
-docker stop vaisw
-docker rm vias
-cd ..
-rm -rf VAISWeb
+docker stop vaisw;
+docker rm vias;
+cd ..;
+rm -rf VAISWeb;
 git clone https://github.com/slapglif/VAISWeb.git;
 cd VAISWeb;
 docker build -t vaisw:latest .;
